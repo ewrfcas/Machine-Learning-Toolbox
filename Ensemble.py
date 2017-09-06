5cd4973e5a1110695821b0e46a993a13e825aab9
ewrfcas <ccjdurandal422@163.com>
2017-09-06 19:41:09 +0800

    bug

--- Ensemble.py
+++ Ensemble.py
@@ -4,7 +4,8 @@ from sklearn.linear_model import LogisticRegression
 
 def stacking_proba(clf,X_train,y,X_test,nfolds=5,random_seed=2017,return_score=False,
                    shuffle=True,metric='acc',clf_name='UnKnown'):
-    folds = StratifiedKFold(n_splits=nfolds, shuffle=shuffle, random_state=random_seed).get_n_splits(X_train,y)
+    folds = StratifiedKFold(n_splits=nfolds, shuffle=shuffle, random_state=random_seed)
+    folds.get_n_splits(X_train,y)
     #return stacking_proba for train set
     train_stacking_proba=np.zeros((X_train.shape[0],np.unique(y).shape[0]))
     score=0
@@ -16,13 +17,13 @@ def stacking_proba(clf,X_train,y,X_test,nfolds=5,random_seed=2017,return_score=F
         y_validate_fold=y[validate_index]
         clf.fit(X_train_fold,y_train_fold)
         fold_preds=clf.predict_proba(X_validate_fold)
-        train_stacking_proba[train_index,:]=fold_preds
+        train_stacking_proba[validate_index,:]=fold_preds
         #validation
         fold_preds_a = np.argmax(fold_preds, axis=1)
         fold_score=len(np.nonzero(y_validate_fold - fold_preds_a == 0)[0]) / len(y_validate_fold)
         print('validate '+metric+":"+str(fold_score))
         score+=fold_score
-    score/=folds
+    score/=nfolds
     #return stacking_proba for test set
     clf.fit(X_train,y)
     test_stacking_proba=clf.predict_proba(X_test)
@@ -38,21 +39,26 @@ def stacking_proba(clf,X_train,y,X_test,nfolds=5,random_seed=2017,return_score=F
 #clfs=list[clf1,clf2,clf3,...]
 #clfs_name=list[name1,name2,...]
 def stacking(clfs,X_train,y,X_test,nfolds=5,stage=1,random_seed=2017,shuffle=True,clfs_name=None,final_clf=None):
+    X_train=np.nan_to_num(X_train)
+    X_test=np.nan_to_num(X_test)
+    column_num = len(np.unique(y))
+    if column_num==2:column_num=1# binary classification only needs one class proba
     for s in range(stage):
         print('stage:'+str(s+1)+'/'+str(stage))
         X_train_stack=[];X_test_stack=[]
-        for i in len(clfs):
+        for i in range(len(clfs)):
             [train_stacking_proba,test_stacking_proba]=stacking_proba(clfs[i],X_train,y,X_test,nfolds,
                                                                       random_seed,shuffle=shuffle,clf_name=clfs_name[i])
-            X_train_stack.append(train_stacking_proba)
-            X_test_stack.append(test_stacking_proba)
-        X_train=np.concatenate(tuple(X_train_stack),axis=0)
-        X_test=np.concatenate(tuple(X_test_stack),axis=0)
+            X_train_stack.append(np.reshape(train_stacking_proba,(train_stacking_proba.shape[0],column_num)))
+            X_test_stack.append(np.reshape(test_stacking_proba,(test_stacking_proba.shape[0],column_num)))
+        X_train=np.concatenate(tuple(X_train_stack),axis=1)
+        X_test=np.concatenate(tuple(X_test_stack),axis=1)
 
     #final_clf default is LogisticRegression
     if final_clf == None:
         final_clf=LogisticRegression()
         final_clf.fit(X_train,y)
+    final_label=final_clf.predict(X_test)
     final_proba=final_clf.predict_proba(X_test)
-    return final_proba
+    return final_label,final_proba
 
