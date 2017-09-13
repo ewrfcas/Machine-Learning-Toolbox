16c734d12eaa0b1e35c3ad4ceaed34a9e0cdcb79
ewrfcas <ccjdurandal422@163.com>
2017-09-13 10:27:14 +0800

    update

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,20 +1,24 @@
 import numpy as np
 
+def MajorityAndMinority(y):
+    labels = np.unique(y)
+    if len(np.where(y == labels[0])[0]) >= len(np.where(y == labels[1])[0]):
+        majority_label = labels[0]
+        majority_num = len(np.where(y == labels[0])[0])
+        minority_label = labels[1]
+        minority_num = y.shape[0] - majority_num
+    else:
+        majority_label = labels[1]
+        majority_num = len(np.where(y == labels[1])[0])
+        minority_label = labels[0]
+        minority_num = y.shape[0] - majority_num
+    return majority_label,majority_num,minority_label,minority_num
+
 #binary
 #ratio: majority'=(majority-minority)*ratio+minoirty
 def RandomUnderSampling(X,y,ratio=0,random_seed=2,replace=False):
     np.random.seed(random_seed)
-    labels=np.unique(y)
-    if len(np.where(y==labels[0])[0])>=len(np.where(y==labels[1])[0]):
-        majority_label=labels[0]
-        majority_num=len(np.where(y==labels[0])[0])
-        minority_label=labels[1]
-        minority_num=y.shape[0]-majority_num
-    else:
-        majority_label=labels[1]
-        majority_num=len(np.where(y==labels[1])[0])
-        minority_label=labels[0]
-        minority_num=y.shape[0]-majority_num
+    majority_label, majority_num, minority_label, minority_num=MajorityAndMinority(y)
     majority_X=X[y==majority_label,:]
     minority_X=X[y==minority_label,:]
     random_index=np.random.choice(np.arange(majority_num),int(minority_num+(majority_num-minority_num)*ratio),replace=replace)
--- Ensemble.py
+++ Ensemble.py
@@ -10,7 +10,7 @@ def stacking_proba(clf,X_train,y,X_test,nfolds=5,random_seed=2017,return_score=F
     train_stacking_proba=np.zeros((X_train.shape[0],np.unique(y).shape[0]))
     score=0
     for i,(train_index, validate_index) in enumerate(folds.split(X_train, y)):
-        print(str(clf_name)+" folds:"+str(i+1)+"/"+str(nfolds))
+        # print(str(clf_name)+" folds:"+str(i+1)+"/"+str(nfolds))
         X_train_fold=X_train[train_index,:]
         y_train_fold=y[train_index]
         X_validate_fold=X_train[validate_index,:]
@@ -21,7 +21,7 @@ def stacking_proba(clf,X_train,y,X_test,nfolds=5,random_seed=2017,return_score=F
         #validation
         fold_preds_a = np.argmax(fold_preds, axis=1)
         fold_score=len(np.nonzero(y_validate_fold - fold_preds_a == 0)[0]) / len(y_validate_fold)
-        print('validate '+metric+":"+str(fold_score))
+        # print('validate '+metric+":"+str(fold_score))
         score+=fold_score
     score/=nfolds
     #return stacking_proba for test set
@@ -38,11 +38,12 @@ def stacking_proba(clf,X_train,y,X_test,nfolds=5,random_seed=2017,return_score=F
 
 #clfs=list[clf1,clf2,clf3,...]
 #clfs_name=list[name1,name2,...]
-def stacking(clfs,X_train,y,X_test,nfolds=5,stage=1,random_seed=2017,shuffle=True,clfs_name=None,final_clf=None):
+def stacking(clfs,X_train,y,X_test,nfolds=5,stage=1,random_seed=2017,shuffle=True,clfs_name=None,final_clf=None,C=1):
     X_train=np.nan_to_num(X_train)
     X_test=np.nan_to_num(X_test)
     column_num = len(np.unique(y))
-    if column_num==2:column_num=1# binary classification only needs one class proba
+    if column_num==2:
+        column_num=1# binary classification only needs one class proba
     for s in range(stage):
         print('stage:'+str(s+1)+'/'+str(stage))
         X_train_stack=[];X_test_stack=[]
@@ -54,11 +55,21 @@ def stacking(clfs,X_train,y,X_test,nfolds=5,stage=1,random_seed=2017,shuffle=Tru
         X_train=np.concatenate(tuple(X_train_stack),axis=1)
         X_test=np.concatenate(tuple(X_test_stack),axis=1)
 
-    #final_clf default is LogisticRegression
+    #final_clf is LogisticRegression as default
     if final_clf == None:
-        final_clf=LogisticRegression()
-        final_clf.fit(X_train,y)
+        final_clf=LogisticRegression(C=C,class_weight='balanced',penalty='l2')
+    final_clf.fit(X_train, y)
     final_label=final_clf.predict(X_test)
     final_proba=final_clf.predict_proba(X_test)
     return final_label,final_proba
+
+#voting (all weights are the same)
+def vote_ensemble(clfs,X_train,y,X_test):
+    confidence=np.zeros(X_test.shape)
+    for clf in clfs:
+        clf.fit(X_train,y)
+        confidence+=(clf.predict_proba(X_test))
+    confidence/=len(clfs)
+    return np.argmax(confidence,axis=0),confidence
+
 
