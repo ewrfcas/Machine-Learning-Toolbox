df42778a9b27b84b34cbb772ccbf7010352a35f5
ewrfcas <ccjdurandal422@163.com>
2017-12-06 16:53:59 +0800

    generator

--- generate_arrays_from_file.py
+++ generate_arrays_from_file.py
@@ -1,7 +1,7 @@
 import numpy as np
 from keras import utils
 import os
-import cv2 as cv
+import h5py
 def generate_arrays_from_file(path):
     while True:
         f = open(path)
@@ -43,4 +43,26 @@ def generate_pics_from_file(path):
                 x = np.reshape(x,(batch_size, 32, 32, 3))
                 yield x, x
                 x = []
+                count = 0
+
+def generate_for_lung(file_list, label_list, batch_size):
+    while True:
+        count = 0
+        x = []
+        y = []
+        for i,path in enumerate(file_list):
+            file=h5py.File(path, 'r')
+            x_temp = file['data']
+            file.close()
+            y_temp = int(label_list[i])
+            count += 1
+            x.append(x_temp)
+            y.append(y_temp)
+            if count == batch_size:
+                x = np.concatenate(x)
+                x = np.resize(x,(batch_size,128,128,128))
+                y = np.array(y)
+                yield x, y
+                x = []
+                y = []
                 count = 0
