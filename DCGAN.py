48fe7b66ca15b3252ec8ca4b26ec3232906879ff
caochenjie <ccjdurandal422@163.com>
2018-01-03 14:19:30 +0800

    DCGAN and reformance_generator

--- DCGAN.py
+++ DCGAN.py
@@ -1,103 +1,69 @@
 from keras.layers import *
 from keras.models import *
 from keras.optimizers import *
+from keras.callbacks import *
 import os
 import numpy as np
-import cv2 as cv
+from skimage import io,transform
 import matplotlib.pyplot as plt
 
 class DCGAN():
-    def __init__(self,image_size=(64,64,1),random_vector_size=100):
+    def __init__(self,image_size=(64,64,3),random_vector_size=100):
         self.image_size=image_size
         self.random_vector_size=random_vector_size
 
-        # discriminator
-        self.discriminator = self.Discriminator()
-        self.optimizer = RMSprop(lr=0.0002, decay=6e-8)
-        self.discriminator.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
-
-        # generator
-        self.generator = self.Generator()
-
         # adversarial
-        self.adversarial = Sequential()
-        self.adversarial.add(self.generator)
-        self.adversarial.add(self.discriminator)
-        self.optimizer = RMSprop(lr=0.0001, decay=3e-8)
+        self.discriminator=self.Discriminator()
+        self.generator=self.Generator()
+        self.adversarial = self.Adversarial(self.discriminator,self.generator)
+        self.optimizer = Adam(lr=0.0002,beta_1=0.5,beta_2=0.999)
         self.adversarial.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        self.check_point = ModelCheckpoint('GAN_model.h5', monitor='loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
 
-    def fit(self,path,batch_size=64,epochs=2000,draw=False):
+    def fit(self,path,batch_size=64,epochs=3000,draw=False):
         train_list=np.array(os.listdir(path))
-        train_index = np.arange(len(train_list))
         self.path=path
-        self.batch_size=batch_size
-        self.epochs=epochs
-        self.batch_index=None
-        for i in range(epochs):
-            print(str(i + 1) + '/' + str(epochs))
-            images_train=self.train_generator(train_list,train_index)
-            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-            images_fake = self.generator.predict(noise)
-            x = np.concatenate((images_train, images_fake), axis=0)
-            y = np.ones([2 * batch_size, 1])
-            y[batch_size:, :] = 0
-            d_loss_acc=self.discriminator.train_on_batch(x, y)
-            y = np.ones([batch_size, 1])
-            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-            g_loss_acc=self.adversarial.train_on_batch(noise, y)
-            print('discriminator_loss/acc:' + str(d_loss_acc) + ' generator_loss/acc:' + str(g_loss_acc))
-            if (i+1)%10==0:
-                if draw:
-                    self.plot_images(path='fake',step=i+1)
+        self.steps=0
+        self.adversarial.fit_generator(generator=self.generate_for_train(train_list,batch_size,random_seed=2000,draw=draw),steps_per_epoch=int(len(train_list)/batch_size),
+                                       epochs=epochs,verbose=1,callbacks=[self.check_point])
+
         return self
 
-    def fit_total(self,X,batch_size=64,epochs=2000,draw=False):
-        train_index = np.arange(X.shape[0])
-        self.X=X
-        self.batch_size=batch_size
-        self.epochs=epochs
-        self.batch_index=None
-        for i in range(epochs):
-            print(str(i + 1) + '/' + str(epochs))
-            images_train=self.train_generator_data(train_index)
-            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-            images_fake = self.generator.predict(noise)
-            x = np.concatenate((images_train, images_fake), axis=0)
-            y = np.ones([2 * batch_size, 1])
-            y[batch_size:, :] = 0
-            d_loss_acc=self.discriminator.train_on_batch(x, y)
-            y = np.ones([batch_size, 1])
-            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
-            g_loss_acc=self.adversarial.train_on_batch(noise, y)
-            print('discriminator_loss/acc:' + str(d_loss_acc) + ' generator_loss/acc:' + str(g_loss_acc))
-            if (i+1)%200==0:
-                if draw:
-                    self.plot_images(path='fake',step=i+1,image_size=self.image_size)
-        return self
-
-    def train_generator(self,train_list,train_index):
-        batch_index=np.random.choice(train_index,size=self.batch_size,replace=False)
-        train_list_batch=train_list[batch_index]
-        images_train=[]
-        for s in train_list_batch:
-            img = cv.imread(self.path + "/" + s,cv.IMREAD_GRAYSCALE)
-            img = cv.resize(img, (self.image_size[0],self.image_size[1]))
-            img=self.preprocess_input(img)
-            images_train.append(img)
-        images_train=np.reshape(images_train, (self.batch_size, self.image_size[0], self.image_size[1], self.image_size[2]))
-        return images_train
-
-    def train_generator_data(self,train_index):
-        batch_index=np.random.choice(train_index,size=self.batch_size,replace=False)
-        train_list_batch=self.X[batch_index,:]
-        images_train=[]
-        for i in range(train_list_batch.shape[0]):
-            img=train_list_batch[i,:]
-            img=cv.resize(img,(self.image_size[0],self.image_size[1]))
-            images_train.append(img)
-        images_train=np.reshape(images_train, (self.batch_size, self.image_size[0], self.image_size[1], self.image_size[2]))
-        images_train=self.preprocess_input_to_tanh(images_train)
-        return images_train
+    # 生成器generator for GAN with shuffle
+    def generate_for_train(self,file_list, batch_size, shuffle=True, random_seed=None, draw=True):
+        while True:
+            # 洗牌
+            if shuffle:
+                if random_seed != None:
+                    random_seed += 1
+                    np.random.seed(random_seed)
+                index = np.arange(file_list.shape[0])
+                np.random.shuffle(index)
+                file_list = file_list[index]
+            count = 0
+            x = []
+            for i, path in enumerate(file_list):
+                img = io.imread(self.path + "/" + path)
+                img = np.array(img)
+                img = transform.resize(img,(self.image_size[0],self.image_size[1],self.image_size[2]))
+                x_temp = self.preprocess_input(img)
+                count += 1
+                x.append(x_temp)
+                if count % batch_size == 0 and count != 0:
+                    x = np.array(x)
+                    x = x.reshape(batch_size, self.image_size[0], self.image_size[1], self.image_size[2]).astype("float32")
+                    noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+                    images_fake = self.generator.predict(noise)
+                    x_D = np.concatenate((x, images_fake), axis=0)
+                    y_D = np.ones(2 * batch_size)
+                    y_D[batch_size:] = 0
+                    x_G = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+                    y_G = np.ones(batch_size)
+                    yield [x_D, x_G], [y_D, y_G]
+                    x = []
+            self.steps+=1
+            if draw:
+                self.plot_images(path='fake', step=self.steps)
 
     def preprocess_input(self,x):
         x = x/255.
@@ -111,22 +77,17 @@ class DCGAN():
         x = x*255.
         return x
 
-    def preprocess_input_to_tanh(self,x):
-        x = x*2.
-        x = x-1.
-        return x
-
-    def plot_images(self, samples=16, noise=None, step=0, image_size=(64, 64, 1), path=None,show=False):
+    def plot_images(self, samples=16, noise=None, step=0, image_size=(64, 64, 3), path=None,show=False):
         if noise is None:
             noise = np.random.uniform(-1.0, 1.0, size=[samples, 100])
-        filename = "fake_step" + str(step) + ".png"
+        filename = "fake_epoch" + str(step) + ".png"
         images = self.preprocess_output(self.generator.predict(noise))
         plt.figure(figsize=(6, 6))
         for i in range(images.shape[0]):
             plt.subplot(4, 4, i + 1)
             image = images[i, :, :, :]
-            image = np.reshape(image, [image_size[0],image_size[1]])
-            plt.imshow(image, cmap='gray')
+            image = np.reshape(image, [image_size[0],image_size[1], image_size[2]])
+            plt.imshow(image)
             plt.axis('off')
         plt.tight_layout()
         if path != None:
@@ -136,25 +97,38 @@ class DCGAN():
         plt.savefig(filename)
         plt.close('all')
 
+    def Adversarial(self,discriminator,generator):
+        input_D = Input(shape=(self.image_size[0], self.image_size[1], self.image_size[2]))
+        output_D = discriminator(input_D)
+
+        input_G = Input(shape=(self.random_vector_size,))
+        output_G = generator(input_G)
+        output_GD = discriminator(output_G)
+
+        return Model(inputs=[input_D,input_G], outputs=[output_D,output_GD], name='Adversarial')
+
     def Discriminator(self):
         input = Input(shape=(self.image_size[0], self.image_size[1], self.image_size[2]))
-        x = Conv2D(32, (3, 3), strides=2, padding='same')(input)
-        x = LeakyReLU(alpha=0.2)(x)
-        x = Dropout(0.3)(x)
-
-        x = Conv2D(64, (3, 3), strides=2, padding='same')(x)
+        x = Conv2D(64, (3, 3), strides=2, padding='same')(input)
         x = LeakyReLU(alpha=0.2)(x)
         x = Dropout(0.3)(x)
 
         x = Conv2D(128, (3, 3), strides=2, padding='same')(x)
+        x = BatchNormalization(momentum=0.9)(x)
         x = LeakyReLU(alpha=0.2)(x)
         x = Dropout(0.3)(x)
 
         x = Conv2D(256, (3, 3), strides=2, padding='same')(x)
+        x = BatchNormalization(momentum=0.9)(x)
         x = LeakyReLU(alpha=0.2)(x)
         x = Dropout(0.3)(x)
 
-        x = Flatten()(x)
+        x = Conv2D(512, (3, 3), strides=2, padding='same')(x)
+        x = BatchNormalization(momentum=0.9)(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Dropout(0.3)(x)
+
+        x = GlobalAveragePooling2D()(x)
         output = Dense(1, activation='sigmoid')(x)
 
         return Model(inputs=input, outputs=output, name='Discriminator')
@@ -163,11 +137,14 @@ class DCGAN():
         dim = int(self.image_size[0] / 16)
         origin_channel = self.image_size[2]
         fake_input = Input(shape=(self.random_vector_size,))
-        x = Dense(dim * dim * 256)(fake_input)
+        x = Dense(dim * dim * 512)(fake_input)
+        x = Activation('relu')(x)
+        x = Reshape((dim, dim, 512))(x)
+
+        x = UpSampling2D()(x)
+        x = Conv2DTranspose(256, (3, 3), padding='same')(x)
         x = BatchNormalization(momentum=0.9)(x)
         x = Activation('relu')(x)
-        x = Reshape((dim, dim, 256))(x)
-        x = Dropout(0.3)(x)
 
         x = UpSampling2D()(x)
         x = Conv2DTranspose(128, (3, 3), padding='same')(x)
@@ -176,11 +153,6 @@ class DCGAN():
 
         x = UpSampling2D()(x)
         x = Conv2DTranspose(64, (3, 3), padding='same')(x)
-        x = BatchNormalization(momentum=0.9)(x)
-        x = Activation('relu')(x)
-
-        x = UpSampling2D()(x)
-        x = Conv2DTranspose(32, (3, 3), padding='same')(x)
         x = BatchNormalization(momentum=0.9)(x)
         x = Activation('relu')(x)
 
---
+++ image_reformance_generator.py
@@ -0,0 +1,94 @@
+from skimage import io
+import numpy as np
+from keras.preprocessing import image
+
+#平移图片x，同时平移关键点y
+def shift(x, y=None, w_limit=0., h_limit=0., row_axis=0, col_axis=1, channel_axis=2, fill_mode='nearest', cval=0.):
+    wshift = np.random.uniform(-1*w_limit, w_limit)
+    hshift = np.random.uniform(-1*h_limit, h_limit)
+    h, w = x.shape[row_axis], x.shape[col_axis] #读取图片的高和宽
+    tx = hshift * h #高偏移大小，若不偏移可设为0，若向上偏移设为正数
+    ty = wshift * w #宽偏移大小，若不偏移可设为0，若向左偏移设为正数
+    translation_matrix = np.array([[1, 0, tx],
+                                  [0, 1, ty],
+                                  [0, 0, 1]])
+    transform_matrix = translation_matrix
+    x = image.apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
+
+    if y is not None:
+        transform_matrix[0:-1, -1] *= -1
+        all_x = np.concatenate((y[0:5], y[10:15], y[20:25], y[30:35]))
+        all_y = np.concatenate((y[5:10], y[15:20], y[25:30], y[35:40]))
+        all_x = all_x.reshape((1, -1))
+        all_y = all_y.reshape((1, -1))
+        all_xy1 = np.concatenate((all_y, all_x, np.ones((1, all_x.shape[1]))), axis=0)
+        y_1 = np.dot(transform_matrix, all_xy1)[0:-1, :]
+        y[0:5] = y_1[1, 0:5]
+        y[5:10] = y_1[0, 0:5]
+        y[10:15] = y_1[1, 5:10]
+        y[15:20] = y_1[0, 5:10]
+        y[20:25] = y_1[1, 10:15]
+        y[25:30] = y_1[0, 10:15]
+        y[30:35] = y_1[1, 15:20]
+        y[35:40] = y_1[0, 15:20]
+
+    return x, y
+
+#旋转图片x,同时旋转关键点y
+def rotate(x, y=None, rotate_limit=0, row_axis=0, col_axis=1, channel_axis=2, fill_mode='nearest', cval=0.):
+    theta = np.pi / 180 * np.random.uniform(-1 * rotate_limit, rotate_limit)  # 逆时针旋转角度
+    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
+                                [np.sin(theta), np.cos(theta), 0],
+                                [0, 0, 1]])
+    h, w = x.shape[row_axis], x.shape[col_axis]
+    transform_matrix = image.transform_matrix_offset_center(rotation_matrix, h, w)
+    x = image.apply_transform(x, transform_matrix, channel_axis, fill_mode, cval)
+
+    if y is not None:
+        all_x = np.concatenate((y[0:5], y[10:15], y[20:25], y[30:35]))
+        all_y = np.concatenate((y[5:10], y[15:20], y[25:30], y[35:40]))
+        all_x = all_x.reshape((1, -1))
+        all_y = all_y.reshape((1, -1))
+        all_xy1 = np.concatenate((all_x, all_y, np.ones((1, all_x.shape[1]))), axis=0)
+        y_1 = np.dot(transform_matrix, all_xy1)[0:-1, :]
+        y[0:5] = y_1[0, 0:5]
+        y[5:10] = y_1[1, 0:5]
+        y[10:15] = y_1[0, 5:10]
+        y[15:20] = y_1[1, 5:10]
+        y[20:25] = y_1[0, 10:15]
+        y[25:30] = y_1[1, 10:15]
+        y[30:35] = y_1[0, 15:20]
+        y[35:40] = y_1[1, 15:20]
+
+    return x, y
+
+#带数据增强（平移，旋转）的生成器generator
+def generate_reformance_kp(file_list, label_list, batch_size, shuffle=True, random_seed=None):
+    while True:
+        if shuffle:
+            if random_seed!=None:
+                random_seed+=1
+                np.random.seed(random_seed)
+            index=np.arange(file_list.shape[0])
+            np.random.shuffle(index)
+            file_list=file_list[index]
+            label_list=label_list[index]
+        count = 0
+        x, y = [], []
+        for i,path in enumerate(file_list):
+            img=io.imread(path)
+            y_temp=label_list[i,:]*224.
+            x_temp = np.reshape(img, (224, 224, 1))
+            x_temp, y_temp = rotate(x_temp, y_temp, 45)#旋转
+            x_temp, y_temp = shift(x_temp, y_temp, 0.15, 0.15)#平移
+            y_temp/=224.
+            x_temp = x_temp.reshape((224,224))
+            count += 1
+            x.append(x_temp)
+            y.append(y_temp)
+            if count % batch_size == 0 and count != 0:
+                x = np.array(x)
+                x = x.reshape(batch_size, 224, 224, 1).astype("float32")
+                y = np.array(y)
+                yield x, y
+                x, y = [], []
