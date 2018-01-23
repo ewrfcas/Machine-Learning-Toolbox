c8c1f5b3a49a8d4893f10b46a3b7fba66f74af73
caochenjie <ccjdurandal422@163.com>
2018-01-23 15:59:18 +0800

    generator bug fix

--- generator_for_imbalance.py
+++ generator_for_imbalance.py
@@ -14,6 +14,7 @@ def generator_train(file_list, label_list, batch_size, shuffle=True, random_seed
             file_list=file_list[index]
             label_list=label_list[index]
         count = 0
+        label_true=keras.utils.to_categorical(np.array(label_list))
         x, y = [], []
         if imbalance:#batch内样本不同类别平衡
             labels=np.unique(label_list)
@@ -28,7 +29,7 @@ def generator_train(file_list, label_list, batch_size, shuffle=True, random_seed
                     if i%len(index_list[j])==0 and i!=0:#当这个类别已经循环了一遍了
                         np.random.shuffle(index_list[j])#需要洗牌这个类别
                     x_temp=io.imread(file_list[index_list[j][i%len(index_list[j])]])
-                    y_temp=label_list[index_list[j][i%len(index_list[j])]]
+                    y_temp=label_true[index_list[j][i%len(index_list[j])],:]
                     count+=1
                     x.append(x_temp)
                     y.append(y_temp)
@@ -36,13 +37,12 @@ def generator_train(file_list, label_list, batch_size, shuffle=True, random_seed
                         x = np.array(x)
                         x = x.reshape(batch_size, 128, 128, 3)
                         y = np.array(y)
-                        y = keras.utils.to_categorical(y)
                         yield x, y
                         x, y = [], []
         else:#for validation
             for i,path in enumerate(file_list):
                 x_temp = io.imread(path)
-                y_temp=label_list[i]
+                y_temp=label_true[i,:]
                 count += 1
                 x.append(x_temp)
                 y.append(y_temp)
@@ -50,6 +50,5 @@ def generator_train(file_list, label_list, batch_size, shuffle=True, random_seed
                     x = np.array(x)
                     x = x.reshape(batch_size, 128, 128, 3)
                     y = np.array(y)
-                    y = keras.utils.to_categorical(y)
                     yield x, y
                     x, y = [], []
