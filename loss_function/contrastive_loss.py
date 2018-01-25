5da1223a167fa946d6a7099289e212f7df71ebc5
caochenjie <ccjdurandal422@163.com>
2018-01-25 17:34:52 +0800

    contrastive_loss

--- loss_function/contrastive_loss.py
+++ loss_function/contrastive_loss.py
@@ -2,15 +2,15 @@ import tensorflow as tf
 
 # contrastive loss
 # loss=(1/2N)*\sum(y*d^2+(1-y)max(alpha-d,0)^2)
-def contrastive_loss(y_same, y_pred, alpha = 0.2):
+def contrastive_loss(y_true, y_pred, alpha = 0.2):
     left = y_pred[0]
     right = y_pred[1]
+    d = tf.sqrt(tf.reduce_sum(tf.square(left - right), 1))
 
-    d = tf.reduce_sum(tf.square(left - right), 1)
-    d_sqrt = tf.sqrt(d)
-
-    loss = y_same * tf.square(tf.maximum(0., alpha - d_sqrt)) + (1 - y_same) * d #y_same=1 while label(left)==label(right) else y_same=0
-
+    # if label(left)==label(right) y_same=1
+    # else y_same=0
+    y_same = tf.cast(tf.equal(y_true[0],y_true[1]),tf.float32)
+    loss = y_same * tf.square(d) + (1-y_same) * tf.square(tf.maximum(alpha - d, 0.))
     loss = 0.5 * tf.reduce_mean(loss)
 
     return loss
