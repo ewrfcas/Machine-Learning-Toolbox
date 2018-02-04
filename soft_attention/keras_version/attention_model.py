94200cf56f5ef3e7a980ae2111a8a9cf09d70c38
caochenjie <ccjdurandal422@163.com>
2018-02-04 23:25:39 +0800

    soft attention

--- soft_attention/keras_version/attention_model.py
+++ soft_attention/keras_version/attention_model.py
@@ -2,45 +2,31 @@ import tensorflow as tf
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
-
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-
-
-def FC_relu(input_dim, output_dim, name='FC_relu'):
-    input_tensor = Input((input_dim,))
-    x = Dense(output_dim)(input_tensor)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    return Model(input_tensor, x, name=name)
+from keras.utils.vis_utils import plot_model
 
 def attention_model(timesteps=101, dim=512, unit=128, n_class=5):
-    fc_relu = FC_relu(dim, int(dim / 2))
-
-    inputs = Input((timesteps, dim))
+    inputs = Input((timesteps, dim),name='input_data')
     # the weights f of timesteps (timesteps * n_class)
-    f_weights = Input((timesteps, n_class))
+    f_weights = Input((timesteps, n_class),name='f_weights')
 
-    x = Lambda(lambda x: tf.unstack(x, axis=1))(inputs)  # len(x)=timestaps
-    xs = []
-    # dim->dim/2
-    for x_i in x:
-        xs.append(fc_relu(x_i))
-    x = Lambda(lambda x: tf.stack(x, axis=1))(xs)  # len(x)=timestaps
+    x = BatchNormalization()(inputs)
     x = LSTM(unit, return_sequences=True)(x)
     x = Reshape((timesteps, unit))(x)
 
     # multiply weights f_weights to the result from LSTM
-    x = Lambda(lambda x:tf.matmul(x[0], x[1], transpose_a=True))([f_weights, x])
+    x = Lambda(lambda x: tf.matmul(x[0], x[1], transpose_a=True),name='fweights_mul')([f_weights, x])
 
     # final multi-softmax
-    x = Lambda(lambda x: tf.unstack(x, axis=1))(x)  # len(x)=timestaps
+    x = Lambda(lambda x: tf.unstack(x, axis=1),name='unstack1')(x)  # len(x)=timestaps
     xs = []
     # each class with different weights w^n
     for x_i in x:
         xs.append(Dense(1)(x_i))
-    x = Lambda(lambda x: tf.stack(x, axis=1))(xs)
+    x = Lambda(lambda x: tf.stack(x, axis=1),name='stack1')(xs)
     x = Reshape((n_class,))(x)
     x = Activation('softmax')(x)
 
-    return Model(inputs=[inputs,f_weights], outputs=x)
+    return Model(inputs=[inputs, f_weights], outputs=x)
+
+model=attention_model()
+plot_model(model,to_file='attention_model.png',show_shapes=False)
