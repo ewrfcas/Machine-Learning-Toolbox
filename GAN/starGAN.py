85b70ab329c0821a7c3707ac8f1c41f1e5d5bfc5
caochenjie <ccjdurandal422@163.com>
2018-02-22 14:22:12 +0800

    d_acc in starGAN

--- GAN/starGAN.py
+++ GAN/starGAN.py
@@ -6,23 +6,25 @@ import keras.backend as K
 import matplotlib.pyplot as plt
 from skimage import io,transform
 from keras.initializers import RandomNormal
-import tensorflow as tf
-#plt.switch_backend('agg')
-conv_init = RandomNormal(0, 0.02)
+# plt.switch_backend('agg')
+# conv_init = RandomNormal(0, 0.02)
 
 class StarGAN():
     def __init__(self, image_size=(128, 128, 3), n_class=5, repeat_num=6, diters=5, D_lr=0.0001, G_lr=0.0001, lamda_gp=10, lamda_cls=1, lamda_rec=10):
         self.image_size = image_size
         self.repeat_num = repeat_num
         self.diters = diters
-        self.n_class=n_class
+        self.n_class = n_class
+        self.D_lr = D_lr
+        self.G_lr = G_lr
+        self.lamda_gp = lamda_gp
+        self.lamda_cls = lamda_cls
+        self.lamda_rec = lamda_rec
 
         # discriminator
         self.discriminator = self.Discriminator()
         # generator
         self.generator = self.Generator()
-        # train step
-        self.D_train, self.G_train = self.starGAN_train(D_lr, G_lr, lamda_gp, lamda_cls, lamda_rec)
 
     def starGAN_train(self, D_lr, G_lr, lamda_gp, lamda_cls, lamda_rec):
 
@@ -37,7 +39,9 @@ class StarGAN():
         d_out_src_real, d_out_cls_real = self.discriminator(x_real)
         d_loss_real = -K.mean(d_out_src_real)
         d_loss_cls = K.mean(K.categorical_crossentropy(label_real, d_out_cls_real))
-        # d_acc, _ = tf.metrics.accuracy(label_real, d_out_cls_real)
+        # cal acc
+        label_pred = K.cast(K.greater(K.clip(d_out_cls_real, 0, 1), 0.5), K.floatx())
+        d_acc = 1 - K.mean(K.clip(K.sum(K.abs(label_real - label_pred), axis=1), 0, 1))
         d_out_src_fake, d_out_cls_fake = self.discriminator(x_fake)
         d_loss_fake = K.mean(d_out_src_fake)
 
@@ -49,8 +53,8 @@ class StarGAN():
         gradient_penalty = K.mean(K.square(x_mixed_gradient_norm - 1))
 
         d_loss = d_loss_real + d_loss_fake + lamda_gp * gradient_penalty + lamda_cls * d_loss_cls
-        d_training_updates = RMSprop(lr=D_lr).get_updates(d_loss, self.discriminator.trainable_weights)
-        D_train = K.function([x_real, label_real, label_real_matrix, label_fake, label_fake_matrix, e], [d_loss], d_training_updates)
+        d_training_updates = Adam(lr=D_lr,beta_1=0.5,beta_2=0.999).get_updates(d_loss, self.discriminator.trainable_weights)
+        D_train = K.function([x_real, label_real, label_real_matrix, label_fake, label_fake_matrix, e], [d_loss, d_acc], d_training_updates)
 
         # loss for generator
         x_rec = self.generator([x_fake, label_real_matrix])
@@ -60,20 +64,24 @@ class StarGAN():
         g_loss_cls = K.mean(K.categorical_crossentropy(label_fake, g_out_cls_fake))
 
         g_loss = g_loss_fake + lamda_rec * g_loss_rec + lamda_cls * g_loss_cls
-
-        g_training_updates = RMSprop(lr=G_lr).get_updates(g_loss, self.generator.trainable_weights)
+        g_training_updates = Adam(lr=G_lr,beta_1=0.5,beta_2=0.999).get_updates(g_loss, self.generator.trainable_weights)
         G_train = K.function([x_real, label_real, label_real_matrix, label_fake, label_fake_matrix], [g_loss], g_training_updates)
 
         return D_train, G_train
 
-    def fit(self, X_path, y, batch_size=16, epochs=50, draw=False, shuffle=True):
+    def fit(self, X_path, y, batch_size=16, epochs=20, draw=False, shuffle=True):
         # X_path:path array[string], y:one-hot array[array[int]]
         train_index = np.arange(len(X_path))
         self.batch_size = batch_size
         self.epochs = epochs
         gen_iterations = 0
         batches = len(train_index) // batch_size
+        D_train, G_train = self.starGAN_train(self.D_lr, self.G_lr, self.lamda_gp, self.lamda_cls, self.lamda_rec)
         for i in range(epochs):
+            # train step
+            if epochs-i<10:
+                D_train, G_train = self.starGAN_train(self.D_lr*((epochs-i)/10), self.G_lr*((epochs-i)/10), self.lamda_gp, self.lamda_cls,
+                                                      self.lamda_rec)
             d_loss_all=0
             d_acc_all=0
             j = 0
@@ -92,9 +100,9 @@ class StarGAN():
                     label_real_matrix=self.label2matrix(label_real)
                     label_fake_matrix=self.label2matrix(label_fake)
                     e = np.random.uniform(size=(batch_size, 1, 1, 1))
-                    [d_loss] = self.D_train([x_real, label_real, label_real_matrix, label_fake, label_fake_matrix, e])
-                    d_loss_all+=d_loss
-                    # d_acc_all+=d_acc
+                    [d_loss,d_acc] = D_train([x_real, label_real, label_real_matrix, label_fake, label_fake_matrix, e])
+                    d_loss_all += d_loss
+                    d_acc_all += d_acc
                     d_print = '\r[steps:%d/%d (diters:%d/%d)] d_loss: %.6f, d_acc: %.4f' % (j, batches, q, self.diters, d_loss_all/j, d_acc_all/j)
                     print(d_print+g_print, end='      ', flush=True)
 
@@ -104,13 +112,13 @@ class StarGAN():
 
                 # train generator
                 gen_iterations += 1
-                [g_loss] = self.G_train([x_real, label_real, label_real_matrix, label_fake, label_fake_matrix])
+                [g_loss] = G_train([x_real, label_real, label_real_matrix, label_fake, label_fake_matrix])
                 g_print = '   [gen_iterations:%d] g_loss: %.6f' % (gen_iterations, g_loss)
                 print(d_print + g_print, end='      ', flush=True)
             print('\n')
         return self
 
-    def train_generator(self,X_path, y, temp_index):
+    def train_generator(self, X_path, y, temp_index, enhance=False):
         X_path=np.array(X_path)
         train_list_batch=X_path[temp_index]
         label_real=y[temp_index,:]
@@ -160,7 +168,7 @@ class StarGAN():
         label_male[:,3]=1
         label_old=label_real
         label_old[:,4]=0
-        # generate blond hair
+        # generate fake image
         x_blond_hair=self.generator.predict([x_real,self.label2matrix(label_blond_hair)])
         x_male=self.generator.predict([x_real,self.label2matrix(label_male)])
         x_old=self.generator.predict([x_real,self.label2matrix(label_old)])
@@ -200,10 +208,10 @@ class StarGAN():
         shortcut = Conv2D(filters, (1, 1), padding='same')(x)
 
         # Residual block
-        x = Conv2D(filters, (3, 3), padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2D(filters, (3, 3), padding='same', use_bias=False)(x)
         x = BatchNormalization()(x, training=1)
         x = Activation('relu')(x)
-        x = Conv2D(filters, (3, 3), padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2D(filters, (3, 3), padding='same', use_bias=False)(x)
         x = BatchNormalization()(x, training=1)
         x = add([x, shortcut])
 
@@ -212,20 +220,20 @@ class StarGAN():
     def Discriminator(self):
         input = Input(shape=(self.image_size[0], self.image_size[1], self.image_size[2]))
 
-        x = Conv2D(64, (4, 4), strides=2, padding='same', kernel_initializer=conv_init)(input)
+        x = Conv2D(64, (4, 4), strides=2, padding='same')(input)
         x = LeakyReLU(alpha=0.01)(x)
 
         k_size=self.image_size[0]//2
         filters=128
         for i in range(self.repeat_num-3):
-            x = Conv2D(filters, (3, 3), strides=2, padding='same', kernel_initializer=conv_init)(x)
+            x = Conv2D(filters, (3, 3), strides=2, padding='same')(x)
             filters*=2
             k_size//=2
             x = LeakyReLU(alpha=0.2)(x)
 
         # k_size*k_size*1 conv filters with valid padding to get 1-d output, which is used to instead of the sigmoid
-        output_x = Conv2D(1, (k_size, k_size), strides=1, padding='valid', kernel_initializer=conv_init, use_bias=False)(x)
-        output_y = Conv2D(self.n_class, (k_size, k_size), strides=1, padding='valid', kernel_initializer=conv_init, use_bias=False)(x)
+        output_x = Conv2D(1, (k_size, k_size), strides=1, padding='valid', use_bias=False)(x)
+        output_y = Conv2D(self.n_class, (k_size, k_size), strides=1, padding='valid', use_bias=False)(x)
         output_x = Flatten()(output_x)
         output_y = Flatten()(output_y)
 
@@ -237,16 +245,16 @@ class StarGAN():
         label_fake = Input(shape=(self.image_size[0], self.image_size[1], self.n_class))
         x = Concatenate(axis=-1)([x_real,label_fake])
 
-        x = Conv2D(64, (5, 5), strides=1, padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2D(64, (5, 5), strides=1, padding='same', use_bias=False)(x)
         x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x, training=1)
         x = Activation('relu')(x)
 
         # down-sampling
-        x = Conv2D(128, (4, 4), strides=2, padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2D(128, (4, 4), strides=2, padding='same', use_bias=False)(x)
         x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x, training=1)
         x = Activation('relu')(x)
 
-        x = Conv2D(256, (4, 4), strides=2, padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2D(256, (4, 4), strides=2, padding='same', use_bias=False)(x)
         x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x, training=1)
         x = Activation('relu')(x)
 
@@ -255,15 +263,15 @@ class StarGAN():
             x = self.ResidualBlock(x, 512)
 
         # up-sampling
-        x = Conv2DTranspose(256, (4, 4), strides=2, padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2DTranspose(256, (4, 4), strides=2, padding='same', use_bias=False)(x)
         x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x, training=1)
         x = Activation('relu')(x)
 
-        x = Conv2DTranspose(128, (4, 4), strides=2, padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2DTranspose(128, (4, 4), strides=2, padding='same', use_bias=False)(x)
         x = BatchNormalization(momentum=0.9, epsilon=1e-5)(x, training=1)
         x = Activation('relu')(x)
 
-        x = Conv2D(origin_channel, (5, 5), strides=1, padding='same', kernel_initializer=conv_init, use_bias=False)(x)
+        x = Conv2D(origin_channel, (5, 5), strides=1, padding='same', use_bias=False)(x)
         output = Activation('tanh')(x)
 
         return Model(inputs=[x_real,label_fake], outputs=output, name='Generator')
