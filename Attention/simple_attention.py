807ab6e7a0d3ef7094a45f75560fdfc786820b2f
ccj <ccjdurandal422@163.com>
2018-04-14 15:04:14 +0800

    attention

--- Attention/simple_attention.py
+++ Attention/simple_attention.py
@@ -3,21 +3,19 @@ import keras.backend as K
 from keras.engine.topology import Layer
 
 class Attention(Layer):
-    def __init__(self, timesteps, attention_size, bias=True, sparsity=False, sparsity_value = 1.,
-                 W_regularizer=regularizers.l1(0.01), W_constraint=None,
-                 U_regularizer=regularizers.l1(0.01), U_constraint=None,
+    def __init__(self, timesteps, bias=True, simple=False,
+                 W_regularizer=None, W_constraint=None,
+                 V_regularizer=None, V_constraint=None,
                  **kwargs):
         self.supports_masking = True
         self.init = initializers.get('glorot_uniform')
         self.bias = bias
         self.timesteps = timesteps
-        self.attention_size = attention_size
-        self.sparsity = sparsity # make the output a more sparsity
-        self.sparsity_value = sparsity_value # while sparsity_value is larger, the outputs is more sparse
+        self.simple = simple
         self.W_regularizer = regularizers.get(W_regularizer)
         self.W_constraint = constraints.get(W_constraint)
-        self.U_regularizer = regularizers.get(U_regularizer)
-        self.U_constraint = constraints.get(U_constraint)
+        self.V_regularizer = regularizers.get(V_regularizer)
+        self.V_constraint = constraints.get(V_constraint)
 
         super(Attention, self).__init__(**kwargs)
 
@@ -25,20 +23,20 @@ class Attention(Layer):
         assert len(input_shape) == 3
 
         self.features_dim = input_shape[-1]
-        self.W = self.add_weight((self.features_dim, self.attention_size),
+        self.W = self.add_weight((self.timesteps, self.timesteps),
                                  initializer=self.init,
                                  regularizer=self.W_regularizer,
                                  constraint=self.W_constraint,
                                  name='{}_W'.format(self.name))
-
-        self.U = self.add_weight((self.attention_size, 1),
-                                 initializer=self.init,
-                                 regularizer=self.W_regularizer,
-                                 constraint=self.W_constraint,
-                                 name='{}_U'.format(self.name))
+        if not self.simple:
+            self.V = self.add_weight((self.features_dim, 1),
+                                     initializer=self.init,
+                                     regularizer=self.V_regularizer,
+                                     constraint=self.V_constraint,
+                                     name='{}_V'.format(self.name))
 
         if self.bias:
-            self.b = self.add_weight((self.attention_size,),
+            self.b = self.add_weight((self.timesteps,),
                                      initializer='zero',
                                      name='{}_b'.format(self.name))
         else:
@@ -50,20 +48,23 @@ class Attention(Layer):
         return None
 
     def call(self, x, mask=None):
-        e = K.dot(x, self.W)
+        x_transpose = K.permute_dimensions(x, (0,2,1))
+        e = K.dot(x_transpose, self.W)
         if self.bias:
             e += self.b
         e = K.tanh(e)
-        e = K.reshape(K.dot(e, self.U), (-1, self.timesteps))
-        if self.sparsity:
-            e = ((e - K.min(e, keepdims=True))/(K.max(e, keepdims=True) - K.min(e, keepdims=True)) - 0.5) * self.sparsity_value
+        if not self.simple:
+            e = K.permute_dimensions(e, (0,2,1))
+            e = K.reshape(K.dot(e, self.V), (-1, self.timesteps))
+        else:
+            e = K.mean(e, axis=1)
         a = K.exp(e)
         if mask is not None:
             a *= K.cast(mask, K.floatx())
         a_weights = a / K.cast(K.sum(a, axis=-1, keepdims=True) + K.epsilon(), K.floatx())
         weighted_output = x * K.expand_dims(a_weights, axis=-1)
 
-        return [K.mean(weighted_output, axis=1), a_weights]
+        return [K.sum(weighted_output, axis=1), a_weights]
 
     def compute_output_shape(self, input_shape):
         return [(input_shape[0], self.features_dim), (input_shape[0], self.timesteps)]
